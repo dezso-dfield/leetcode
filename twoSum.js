815cb782549875cc4af191e99a54ebee6c930544
Dezso Mezo | DField Solutions <dezso@dfieldsolutions.com>
2025-10-10 11:06:08 +0200

    Create twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,21 @@
+// O(n2) Solution
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let result = []
+    for (let i = 0;i < nums.length;i++) {
+        for (let j = i+1;j < nums.length;j++) {
+            if (nums[i] + nums[j] == target) {
+                result.push(i);
+                result.push(j);
+            }
+        }
+    }
+    return result;
+};
+
+//later will optimize it...
