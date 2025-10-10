ee5a3eeb0cf0c9303c4d681e3b9b1c5b14fca880
Dezso Mezo | DField Solutions <dezso@dfieldsolutions.com>
2025-10-10 21:47:34 +0200

    Create lengthOfLastWord.cpp

---
+++ lengthOfLastWord.cpp
@@ -0,0 +1,17 @@
+// O(1) runtime
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        for (int i = s.size()-1; i >= 0; i--) {
+            if (count > 0 && s[i] == ' ') {
+                return count;
+            }
+            if (s[i] != ' ') {
+                count += 1;
+            }
+        }
+        return count;
+    }
+};
