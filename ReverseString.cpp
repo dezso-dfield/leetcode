143ea9bdd6723a5810ff19ddfc1b97fb0c00c390
Dezso Mezo | DField Solutions <dezso@dfieldsolutions.com>
2025-10-17 22:49:19 +0200

    Create ReverseString.cpp

---
+++ ReverseString.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l = 0;
+        int r = s.size()-1;
+        while (l <= r) {
+            int temp = s[r];
+            s[r] = s[l];
+            s[l] = temp;
+            l++;
+            r--;
+        }
+    }
+};
